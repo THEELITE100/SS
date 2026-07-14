f5cc31ef530a7ea94cd51d4f7d6a05710604fa6b
Sumit Parashar <parasharsumit730@gmail.com>
2026-07-14 14:37:13 +0530

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: ['http://localhost:5173', 'https://ss-two-pi.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
   })
